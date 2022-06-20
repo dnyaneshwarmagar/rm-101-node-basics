3d9a54f798645267121e9a5a32da7f5921d5212c
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-06-20 19:16:52 +0530

    fc

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 // install and import express
-const express = () => {};
+const express = () => {
+    return require("express")()
+};
+// const express = require("express")
 let app = express();
+const path = require("path")
 
 // Code here
+app.get("/",(req,res)=>{
+    try {      
+        return res.status(200).sendFile(path.join(__dirname,'/assets/users.html'))
+      } catch (err) {
+        return res.status(500).send({ message: err.message });
+      }
+})
 
+console.log("dir",__dirname)
+app.listen(8000,()=>{
+    try{
+        console.log("listening on port 8000!")
+    }catch(err){
+        console.log(err)
+    }
+})
 // Note: Do not remove this export statement
 module.exports = app;
